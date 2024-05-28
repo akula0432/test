4c2375649bb7773560435f7714f0478362904eef
akula0432 <140506740+akula0432@users.noreply.github.com>
2024-05-28 11:16:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import TwoWeeksComp from "./TwoWeeksComp";
 import ThreeWeeksComp from "./ThreeWeeksComp";
 
 function App() {
-  const startDate = new Date(2024, 4, 22, 9);
-  const endDate2Weeks = new Date(2024, 5, 2, 17, 30);
-  const endDate3Weeks = new Date(2024, 5, 9, 17, 30);
-  const endDateMonth = new Date(2024, 5, 19, 17, 30);
+  const startDate = new Date(2024, 4, 28, 12);
+  const endDate2Weeks = new Date(2024, 5, 11, 12);
+  const endDate3Weeks = new Date(2024, 5, 18, 12);
+  const endDateMonth = new Date(2024, 5, 28, 12);
 
   return (
     <div className="App">
       <div>
         <div className="start">Start date:</div>
-        19 May 17:30
+        28 May 12:00
       </div>
       <TwoWeeksComp startDate={startDate} endDate2Weeks={endDate2Weeks} />
       <ThreeWeeksComp startDate={startDate} endDate3Weeks={endDate3Weeks} />
