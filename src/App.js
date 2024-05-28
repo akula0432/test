86ef59420dd084d208f47ba526e97c9ab5da3733
akula <akula>
2024-05-28 11:26:56 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import TwoWeeksComp from "./TwoWeeksComp";
 import ThreeWeeksComp from "./ThreeWeeksComp";
 
 function App() {
-  const startDate = new Date(2024, 4, 28, 12);
-  const endDate2Weeks = new Date(2024, 5, 11, 12, 0);
-  const endDate3Weeks = new Date(2024, 5, 18, 12, 0);
-  const endDateMonth = new Date(2024, 5, 28, 12, 0);
+  const startDate = new Date(2024, 4, 28, 12, 30);
+  const endDate2Weeks = new Date(2024, 5, 11, 12, 30);
+  const endDate3Weeks = new Date(2024, 5, 18, 12, 30);
+  const endDateMonth = new Date(2024, 5, 28, 12, 30);
 
   return (
     <div className="App">
       <div>
         <div className="start">Start date:</div>
-        28 May 12:00
+        28 May 12:30
       </div>
       <TwoWeeksComp startDate={startDate} endDate2Weeks={endDate2Weeks} />
       <ThreeWeeksComp startDate={startDate} endDate3Weeks={endDate3Weeks} />
